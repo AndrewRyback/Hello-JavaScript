69a5ffc9d3c6eca11e8eb5ceb1e31858057bd4cb
Andrii Rybak <andrey.r88816@gmail.com>
2024-07-31 20:46:54 +0300

    edit

--- task-max.js
+++ task-max.js
@@ -13,7 +13,7 @@ let speed = distance / time;
 alert(`Необхідно рухатися зі швидкістю ${speed.toFixed(2)} км/год, щоб встигнути вчасно`);
 
 
-const EXCHANGE_RATE = 0.91; // Курс доларів до євро
+const EXCHANGE_RATE = 0.91; 
 let dollars = prompt("Вкажи суму в доларах:");
 let euros = dollars * EXCHANGE_RATE;
 alert(`${dollars} доларів дорівнює ${euros.toFixed(2)} євро`);
